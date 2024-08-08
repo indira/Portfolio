07db28a937c347d75cc871dc9883c40bcd1590ad
Indira Pandey <indubhusal@gmail.com>
2024-08-07 20:11:00 -0700

    Added flash message to the create post page.

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -1,12 +1,16 @@
 import React, { useState, useContext } from "react"
+import { useNavigate } from "react-router-dom"
 import Page from "../Page/Page"
 import Axios from "axios"
 import StateContext from "../../StateContext"
+import DispatchContext from "../../DispatchContext"
 
 function CreatePost() {
+  const appDispatch = useContext(DispatchContext)
   const [title, setTitle] = useState("")
   const [content, setContent] = useState("")
   const appState = useContext(StateContext)
+  const navigate = useNavigate()
 
   function handleSubmit(e) {
     e.preventDefault()
@@ -27,12 +31,14 @@ function CreatePost() {
             }
           }
         )
-        console.log(response.data)
+        const postId = response.data.id
+        navigate(`/posts/${postId}`)
+        appDispatch({ type: "flashMessage", value: { text: "Congratulations, you successfully created a post." } })
       } catch (e) {
         if (e.response && e.response.status === 403) {
-          console.log("Permission denied or invalid token")
+          appDispatch({ type: "flashMessage", value: { text: "Permission denied or invalid token" } })
         } else {
-          console.log("There was a problem", e.message)
+          appDispatch({ type: "flashMessage", value: { text: "There was a problem." } })
         }
       }
     }
@@ -44,7 +50,7 @@ function CreatePost() {
       <div className="container__narrow wrapper wrapper__border">
         <form onSubmit={handleSubmit}>
           <label htmlFor="post-title">Post Title</label>
-          <input autoFocus name="title" id="post-title" type="text" value={title} onChange={e => setTitle(e.target.value)} placeholder="" autoComplete="off" />
+          <input onChange={e => setTitle(e.target.value)} autoFocus name="title" id="post-title" type="text" value={title} placeholder="" autoComplete="off" />
           <label htmlFor="post-body">Post Content</label>
           <textarea name="body" id="post-body" value={content} onChange={e => setContent(e.target.value)} type="text" />
           <button className="button button--blue">Create Post</button>
